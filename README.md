989d499b8a0fff9dabe14a44ab162d93d11e571a
Sergey Buterbrodov <butergut@gmail.com>
2019-12-30 12:36:08 +0300

    test commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# calc
+
+Hello World!
+
+
---
+++ calc_on_tk_v_0_02.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+root = tk.Tk()
+top_frame = tk.Frame(root)
+bottom_frame = tk.Frame(root)
+bottom_frame.pack(side=tk.BOTTOM)
+top_frame.pack(side=tk.TOP)
+lab = tk.Label(top_frame, bg='black', fg='green', width=20)
+entry1 = tk.Entry(top_frame)
+entry2 = tk.Entry(top_frame)
+
+
+def div(event):
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        lab['text'] = num1 / num2
+
+    except:
+        lab['text'] = 'Ошибка'
+
+def mult(event):
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        lab['text'] = num1 * num2
+
+    except:
+        lab['text'] = 'Ошибка'
+
+
+def degree(event):
+    try:
+        num1 = float(entry1.get())
+        num2 = float(entry2.get())
+        lab['text'] = num1 ** num2
+
+    except:
+        lab['text'] = 'Ошибка'
+
+
+button_degree = tk.Button(bottom_frame, text='**')
+button_degree.bind('<Button-1>', degree)
+button_mult = tk.Button(bottom_frame, text="*")
+button_mult.bind('<Button-1>', mult)
+button_div = tk.Button(bottom_frame, text="/")
+button_div.bind('<Button-1>', div)
+button_degree.pack(side=tk.RIGHT)
+button_div.pack(side=tk.RIGHT)
+button_mult.pack(side=tk.LEFT)
+lab.pack()
+entry1.pack()
+entry2.pack()
+
+root.mainloop()
